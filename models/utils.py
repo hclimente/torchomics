2af773eb662b57154eedc00ed1d0d0cfa6bf4409
Héctor Climente-González <hclimente@gmail.com>
2022-05-05 17:52:29 +0900

    fixed training pipeline
    added validation and dev dataset
    
    improved readability

--- models/utils.py
+++ models/utils.py
@@ -1,6 +1,7 @@
 import random
 
 import numpy as np
+import scipy
 import torch
 
 
@@ -8,3 +9,19 @@ def fix_seeds(seed):
     torch.manual_seed(seed)
     random.seed(seed)
     np.random.seed(seed)
+
+
+def pearsonr(x, y):
+
+    x = x.cpu().detach().numpy().flatten()
+    y = y.cpu().detach().numpy().flatten()
+
+    return scipy.stats.pearsonr(x, y)[0]
+
+
+def spearmanr(x, y):
+
+    x = x.cpu().detach().numpy()
+    y = y.cpu().detach().numpy()
+
+    return scipy.stats.spearmanr(x, y)[0]
