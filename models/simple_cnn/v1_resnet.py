00c3f33f2025c04bd1bf7a74edd6cc94b2d0718e
jcboyd <joseph.boyd@centralesupelec.fr>
2022-06-15 12:25:30 +0200

    updated resnets

--- models/simple_cnn/v1_resnet.py
+++ models/simple_cnn/v1_resnet.py
@@ -4,6 +4,39 @@ import torch.nn as nn
 from models.layers import RevCompConv1D
 
 
+class BasicBlock(nn.Module):
+    def __init__(
+        self,
+        channels_in,
+        channels_bn,
+        channels_out,
+        stride,
+        groups,
+        downsample=None,
+    ):
+        super(BasicBlock, self).__init__()
+
+        self.downsample = downsample
+
+        self.bottleneck = nn.Sequential(
+            nn.Conv1d(
+                channels_in, channels_bn, 1, bias=False, stride=stride, groups=groups
+            ),
+            nn.BatchNorm1d(channels_bn),
+            nn.ReLU(),
+            nn.Conv1d(channels_bn, channels_out, 1, bias=False, groups=groups),
+            nn.BatchNorm1d(channels_out),
+        )
+
+    def forward(self, x):
+
+        identity = x if self.downsample is None else self.downsample(x)
+        x = identity + self.bottleneck(x)
+        x = nn.functional.relu(x)
+
+        return x
+
+
 class Bottleneck(nn.Module):
     def __init__(
         self,
@@ -61,18 +94,24 @@ class ConvNeXtBottleneck(nn.Module):
         self.downsample = downsample
 
         self.bottleneck = nn.Sequential(
-            nn.Conv1d(channels_in, channels_in, 7, bias=False, groups=channels_in),
-            nn.LayerNorm1d(channels_in),
             nn.Conv1d(
-                4 * channels_in,
-                4 * channels_in,
+                channels_in,
+                channels_in,
+                7,
+                padding="same",
+                bias=False,
+                groups=channels_in,
+            ),
+            nn.LayerNorm(channels_in),
+            nn.Conv1d(
+                channels_in,
+                channels_bn,
                 1,
-                padding=1,
                 bias=False,
                 stride=stride,
             ),
             nn.GELU(),
-            nn.Conv1d(4 * channels_in, channels_out, 1, bias=False),
+            nn.Conv1d(channels_bn, channels_out, 1, bias=False),
         )
 
     def forward(self, x):
@@ -97,18 +136,18 @@ class ResNet(pl.LightningModule):
         )
 
         self.layer1 = self._make_layer(
-            layers[0], block, 64, 64, stride=1, groups=groups
+            block, layers[0], 64, 64, stride=1, groups=groups
         )
         self.layer2 = self._make_layer(
-            layers[1], block, 128, 256, stride=2, groups=groups
+            block, layers[1], 128, 256, stride=2, groups=groups
         )
         self.layer3 = self._make_layer(
-            layers[2], block, 256, 1024, stride=2, groups=groups
+            block, layers[2], 256, 1024, stride=2, groups=groups
         )
         self.layer4 = self._make_layer(
-            layers[3], block, 512, 2048, stride=2, groups=groups
+            block, layers[3], 512, 2048, stride=2, groups=groups
         )
-        self.avg_pool = nn.AdaptiveAvgPool1d(2048)
+        self.avg_pool = nn.AdaptiveAvgPool1d(1)
 
         self.fc = nn.Linear(2048, 1)
 
@@ -123,7 +162,9 @@ class ResNet(pl.LightningModule):
         x = self.fc(x)
         return x
 
-    def _make_layer(self, block, nb_repeats, channels_bn, channels_out, stride=1):
+    def _make_layer(
+        self, block, nb_repeats, channels_bn, channels_out, stride=1, groups=1
+    ):
 
         downsample = None
         layers = []
@@ -136,16 +177,24 @@ class ResNet(pl.LightningModule):
 
         for _ in range(nb_repeats):
             layers.append(
-                block(self.channels_in, channels_bn, channels_out, stride, downsample)
+                block(
+                    self.channels_in,
+                    channels_bn,
+                    channels_out,
+                    stride,
+                    groups,
+                    downsample,
+                )
             )
-
-        self.channels_in = channels_out
+            downsample = None  # downsampling only occurs in the first block
+            stride = 1
+            self.channels_in = channels_out
 
         return nn.Sequential(*layers)
 
 
 def ResNet18():
-    return ResNet([2, 2, 2, 2], Bottleneck)
+    return ResNet([2, 2, 2, 2], BasicBlock)
 
 
 def ResNet50():
@@ -153,15 +202,11 @@ def ResNet50():
 
 
 def ResNeXt18():
-    return ResNet([2, 2, 2, 2], Bottleneck, groups=32)
+    return ResNet([2, 2, 2, 2], BasicBlock, groups=32)
 
 
 def ResNeXt50():
     return ResNet([3, 4, 6, 3], Bottleneck, groups=32)
-
-
-def ConvNeXt18():
-    return ResNet([2, 2, 2, 2], ConvNeXtBottleneck, groups=32)
 
 
 def ConvNeXt50():
