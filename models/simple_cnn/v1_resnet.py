47b30a9da6d965e0bb6d6321062bd481618099c1
jcboyd <joseph.boyd@centralesupelec.fr>
2022-06-15 16:22:15 +0200

    finalised resnets

--- models/simple_cnn/v1_resnet.py
+++ models/simple_cnn/v1_resnet.py
@@ -5,11 +5,13 @@ from models.layers import RevCompConv1D
 
 
 class BasicBlock(nn.Module):
+
+    expansion: int = 1
+
     def __init__(
         self,
         channels_in,
-        channels_bn,
-        channels_out,
+        width,
         stride,
         groups,
         downsample=None,
@@ -19,13 +21,11 @@ class BasicBlock(nn.Module):
         self.downsample = downsample
 
         self.bottleneck = nn.Sequential(
-            nn.Conv1d(
-                channels_in, channels_bn, 1, bias=False, stride=stride, groups=groups
-            ),
-            nn.BatchNorm1d(channels_bn),
+            nn.Conv1d(channels_in, width, 1, bias=False, stride=stride, groups=groups),
+            nn.BatchNorm1d(width),
             nn.ReLU(),
-            nn.Conv1d(channels_bn, channels_out, 1, bias=False, groups=groups),
-            nn.BatchNorm1d(channels_out),
+            nn.Conv1d(width, width, 1, bias=False, groups=groups),
+            nn.BatchNorm1d(width),
         )
 
     def forward(self, x):
@@ -38,11 +38,13 @@ class BasicBlock(nn.Module):
 
 
 class Bottleneck(nn.Module):
+
+    expansion: int = 4
+
     def __init__(
         self,
         channels_in,
-        channels_bn,
-        channels_out,
+        width,
         stride,
         groups,
         downsample=None,
@@ -52,22 +54,22 @@ class Bottleneck(nn.Module):
         self.downsample = downsample
 
         self.bottleneck = nn.Sequential(
-            nn.Conv1d(channels_in, channels_bn, 1, bias=False),
-            nn.BatchNorm1d(channels_bn),
+            nn.Conv1d(channels_in, width, 1, bias=False),
+            nn.BatchNorm1d(width),
             nn.ReLU(),
             nn.Conv1d(
-                channels_bn,
-                channels_bn,
+                width,
+                width,
                 3,
                 padding=1,
                 bias=False,
                 stride=stride,
                 groups=groups,
             ),
-            nn.BatchNorm1d(channels_bn),
+            nn.BatchNorm1d(width),
             nn.ReLU(),
-            nn.Conv1d(channels_bn, channels_out, 1, bias=False),
-            nn.BatchNorm1d(channels_out),
+            nn.Conv1d(width, width * self.expansion, 1, bias=False),
+            nn.BatchNorm1d(width * self.expansion),
         )
 
     def forward(self, x):
@@ -80,11 +82,13 @@ class Bottleneck(nn.Module):
 
 
 class ConvNeXtBottleneck(nn.Module):
+
+    expansion: int = 1
+
     def __init__(
         self,
         channels_in,
-        channels_bn,
-        channels_out,
+        width,
         stride,
         groups,
         downsample=None,
@@ -93,25 +97,27 @@ class ConvNeXtBottleneck(nn.Module):
 
         self.downsample = downsample
 
+        next_width = int(self.expansion * width)
+
         self.bottleneck = nn.Sequential(
             nn.Conv1d(
                 channels_in,
-                channels_in,
-                7,
+                next_width,
+                kernel_size=7,
                 padding="same",
                 bias=False,
                 groups=channels_in,
             ),
-            nn.LayerNorm(channels_in),
+            nn.GroupNorm(num_groups=1, num_channels=next_width),
             nn.Conv1d(
-                channels_in,
-                channels_bn,
-                1,
+                next_width,
+                4 * next_width,
+                kernel_size=1,
                 bias=False,
                 stride=stride,
             ),
             nn.GELU(),
-            nn.Conv1d(channels_bn, channels_out, 1, bias=False),
+            nn.Conv1d(4 * next_width, next_width, 1, bias=False),
         )
 
     def forward(self, x):
@@ -123,10 +129,10 @@ class ConvNeXtBottleneck(nn.Module):
 
 
 class ResNet(pl.LightningModule):
-    def __init__(self, layers, block, groups=1):
+    def __init__(self, layers, block, base_width=64, groups=1):
         super(ResNet, self).__init__()
 
-        self.channels_in = 64
+        self.channels_in = base_width
 
         self.input = nn.Sequential(
             RevCompConv1D(4, self.channels_in, 15, padding="same", bias=False),
@@ -136,51 +142,56 @@ class ResNet(pl.LightningModule):
         )
 
         self.layer1 = self._make_layer(
-            block, layers[0], 64, 64, stride=1, groups=groups
+            block, layers[0], base_width, stride=1, groups=groups
         )
         self.layer2 = self._make_layer(
-            block, layers[1], 128, 256, stride=2, groups=groups
+            block, layers[1], 2 * base_width, stride=2, groups=groups
         )
         self.layer3 = self._make_layer(
-            block, layers[2], 256, 1024, stride=2, groups=groups
+            block, layers[2], 4 * base_width, stride=2, groups=groups
         )
         self.layer4 = self._make_layer(
-            block, layers[3], 512, 2048, stride=2, groups=groups
+            block, layers[3], 8 * base_width, stride=2, groups=groups
         )
         self.avg_pool = nn.AdaptiveAvgPool1d(1)
 
-        self.fc = nn.Linear(2048, 1)
+        self.fc = nn.Linear(8 * base_width * block.expansion, 1)
 
     def forward(self, x, rc=None):
+
         x = self.input(x)
+
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
         x = self.avg_pool(x)
+
         x = x.reshape(x.shape[0], -1)
         x = self.fc(x)
+
         return x
 
-    def _make_layer(
-        self, block, nb_repeats, channels_bn, channels_out, stride=1, groups=1
-    ):
+    def _make_layer(self, block, nb_repeats, width, stride=1, groups=1):
 
         downsample = None
         layers = []
 
-        if stride != 1 or self.channels_in != channels_out:
+        expanded_width = width * block.expansion
+
+        if stride != 1 or self.channels_in != expanded_width:
             downsample = nn.Sequential(
-                nn.Conv1d(self.channels_in, channels_out, 1, stride=stride, bias=False),
-                nn.BatchNorm1d(channels_out),
+                nn.Conv1d(
+                    self.channels_in, expanded_width, 1, stride=stride, bias=False
+                ),
+                nn.BatchNorm1d(expanded_width),
             )
 
         for _ in range(nb_repeats):
             layers.append(
                 block(
                     self.channels_in,
-                    channels_bn,
-                    channels_out,
+                    width,
                     stride,
                     groups,
                     downsample,
@@ -188,7 +199,7 @@ class ResNet(pl.LightningModule):
             )
             downsample = None  # downsampling only occurs in the first block
             stride = 1
-            self.channels_in = channels_out
+            self.channels_in = expanded_width
 
         return nn.Sequential(*layers)
 
@@ -210,4 +221,4 @@ def ResNeXt50():
 
 
 def ConvNeXt50():
-    return ResNet([3, 4, 6, 3], ConvNeXtBottleneck, groups=32)
+    return ResNet([3, 4, 6, 3], ConvNeXtBottleneck, groups=32, base_width=96)
