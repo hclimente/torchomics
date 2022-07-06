429c0df87ac212923040dcd7e5b7287de184aef8
Héctor Climente-González <hclimente@gmail.com>
2022-07-06 21:08:18 +0900

    added mixup experiments

--- scripts/sge_train.sh
+++ scripts/sge_train.sh
@@ -4,15 +4,12 @@
 #$ -j y
 #$ -jc gpu-container_g8.24h
 #$ -ac d=nvcr-pytorch-2204
-#$ -t 1-10
+#$ -t 3-5
 
 . /fefs/opt/dgx/env_set/nvcr-pytorch-2204.sh
 export PYTHONPATH=/home/hclimente/projects/dna2prot:${PYTHONPATH}
 
 DROPOUT=`echo "print($SGE_TASK_ID/10.0)" | python`
-SEED=2
+SEED=4
 
-python scripts/train.py -kernel_size 3 -nb_repeats 4 -p_dropout $DROPOUT -seed $SEED
-python scripts/train.py -kernel_size 7 -nb_repeats 8 -p_dropout $DROPOUT -seed $SEED
-python scripts/train.py -kernel_size 9 -nb_repeats 8 -p_dropout $DROPOUT -seed $SEED
-python scripts/train.py -kernel_size 9 -nb_repeats 9 -p_dropout $DROPOUT -seed $SEED
+python scripts/train.py -kernel_size 3 -nb_repeats 4 -p_dropout $DROPOUT -seed $SEED -transforms mixup
