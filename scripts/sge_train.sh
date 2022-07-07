4d55e441f11c27183940079ee566496cfaf1c7e1
Héctor Climente-González <hclimente@gmail.com>
2022-07-07 18:28:41 +0900

    use different mixup alphas

--- scripts/sge_train.sh
+++ scripts/sge_train.sh
@@ -4,12 +4,12 @@
 #$ -j y
 #$ -jc gpu-container_g8.24h
 #$ -ac d=nvcr-pytorch-2204
-#$ -t 3-5
+#$ -t 1-3
 
 . /fefs/opt/dgx/env_set/nvcr-pytorch-2204.sh
 export PYTHONPATH=/home/hclimente/projects/dna2prot:${PYTHONPATH}
 
-DROPOUT=`echo "print($SGE_TASK_ID/10.0)" | python`
-SEED=4
+ALPHA=`echo "print(10**($SGE_TASK_ID-2))" | python`
+SEED=2
 
-python scripts/train.py -kernel_size 3 -nb_repeats 4 -p_dropout $DROPOUT -seed $SEED -transforms mixup
+python scripts/train.py -kernel_size 3 -nb_repeats 4 -p_dropout 0 -alpha $ALPHA -seed $SEED -transforms mixup
