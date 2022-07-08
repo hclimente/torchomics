9af25c7240b7efe88533642194d6422de1144491
Héctor Climente-González <hclimente@gmail.com>
2022-07-08 13:19:58 +0900

    DenseNet results added

--- scripts/sge_train.sh
+++ scripts/sge_train.sh
@@ -4,15 +4,9 @@
 #$ -j y
 #$ -jc gpu-container_g8.24h
 #$ -ac d=nvcr-pytorch-2204
-#$ -t 1-10
+#$ -t 1-5
 
 . /fefs/opt/dgx/env_set/nvcr-pytorch-2204.sh
 export PYTHONPATH=/home/hclimente/projects/dna2prot:${PYTHONPATH}
 
-DROPOUT=`echo "print($SGE_TASK_ID/10.0)" | python`
-SEED=2
-
-python scripts/train.py -kernel_size 3 -nb_repeats 4 -p_dropout $DROPOUT -seed $SEED
-python scripts/train.py -kernel_size 7 -nb_repeats 8 -p_dropout $DROPOUT -seed $SEED
-python scripts/train.py -kernel_size 9 -nb_repeats 8 -p_dropout $DROPOUT -seed $SEED
-python scripts/train.py -kernel_size 9 -nb_repeats 9 -p_dropout $DROPOUT -seed $SEED
+python scripts/train.py -seed $SGE_TASK_ID -growth_rate 10 -nb_repeats 30
