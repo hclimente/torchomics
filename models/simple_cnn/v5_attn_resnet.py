b68b1a54567423f9daa94bd3cc088b20b22ce086
jcboyd <joseph.boyd@centralesupelec.fr>
2022-07-20 14:53:51 +0200

    added multihead attention, ConvNeXt26

--- models/simple_cnn/v5_attn_resnet.py
+++ models/simple_cnn/v5_attn_resnet.py
@@ -3,6 +3,7 @@ import math
 import torch
 import torch.nn as nn
 
+from models.layers import RevCompConv1D
 from models.simple_cnn.v1_resnet import BasicBlock, Bottleneck, ResNet
 
 
@@ -10,7 +11,10 @@ class SelfAttention(nn.Module):
 
     """Self attention Layer"""
 
-    def __init__(self, in_dim):
+    def __init__(
+        self,
+        in_dim,
+    ):
 
         super(SelfAttention, self).__init__()
 
@@ -41,33 +45,56 @@ class SelfAttention(nn.Module):
         out = torch.bmm(proj_value, attention.permute(0, 2, 1))
         out = self.gamma * out + x
 
-        return out, attention
+        return out
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, embed_dim, num_heads=8):
+
+        super(MultiHeadAttention, self).__init__()
+        self.attn = nn.MultiheadAttention(embed_dim, num_heads, batch_first=True)
+
+    def forward(self, x):
+
+        x = x.permute((0, 2, 1))
+        x = self.attn(x, x, x, need_weights=False)[0]
+        x = x.permute((0, 2, 1))
+
+        return x
 
 
 class AttentionResNet(ResNet):
-    def __init__(self, layers, block, base_width=64, groups=1):
+    def __init__(self, layers, block, attention_block, base_width=64, groups=1):
         super(AttentionResNet, self).__init__(layers, block, base_width=64, groups=1)
 
-        self.attn1 = SelfAttention(base_width * block.expansion)
-        self.attn2 = SelfAttention(2 * base_width * block.expansion)
-        self.attn3 = SelfAttention(4 * base_width * block.expansion)
-        self.attn4 = SelfAttention(8 * base_width * block.expansion)
+        self.input = nn.Sequential(
+            RevCompConv1D(4, base_width, 15, padding="same", bias=False),
+            nn.BatchNorm1d(base_width),
+            nn.ReLU(),
+        )
+
+        self.maxpool = nn.MaxPool1d(2)
+        self.attn1 = attention_block(base_width)
+        self.attn2 = attention_block(base_width * block.expansion)
+        self.attn3 = attention_block(2 * base_width * block.expansion)
+        self.attn4 = attention_block(4 * base_width * block.expansion)
 
     def forward(self, x, rc=None):
 
         x = self.input(x)
+        x = self.maxpool(x)
 
+        x = self.attn1(x)
         x = self.layer1(x)
-        x = self.attn1(x)[0]
 
+        x = self.attn2(x)
         x = self.layer2(x)
-        x = self.attn2(x)[0]
 
+        x = self.attn3(x)
         x = self.layer3(x)
-        # x = self.attn3(x)[0]
 
+        x = self.attn4(x)
         x = self.layer4(x)
-        # x = self.attn4(x)[0]
 
         x = self.avg_pool(x)
 
@@ -79,9 +106,19 @@ class AttentionResNet(ResNet):
 
 class AttentionResNet18(AttentionResNet):
     def __init__(self):
-        super().__init__([2, 2, 2, 2], BasicBlock)
+        super().__init__([2, 2, 2, 2], BasicBlock, SelfAttention)
 
 
 class AttentionResNet50(AttentionResNet):
     def __init__(self):
-        super().__init__([3, 4, 6, 3], Bottleneck)
+        super().__init__([3, 4, 6, 3], Bottleneck, SelfAttention)
+
+
+class MultiHeadAttentionResNet18(AttentionResNet):
+    def __init__(self):
+        super().__init__([2, 2, 2, 2], BasicBlock, MultiHeadAttention)
+
+
+class MultiHeadAttentionResNet50(AttentionResNet):
+    def __init__(self):
+        super().__init__([3, 4, 6, 3], Bottleneck, MultiHeadAttention)
