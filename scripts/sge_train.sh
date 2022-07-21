16a6006ca1fdb1afc2b63703fe75b2eafe5d9692
Héctor Climente-González <hclimente@gmail.com>
2022-07-21 11:33:40 +0900

    added resnet and deeplstm results

--- scripts/sge_train.sh
+++ scripts/sge_train.sh
@@ -4,9 +4,9 @@
 #$ -j y
 #$ -jc gpu-container_g8.24h
 #$ -ac d=nvcr-pytorch-2204
-#$ -t 1-5
+#$ -t 3-3
 
 . /fefs/opt/dgx/env_set/nvcr-pytorch-2204.sh
 export PYTHONPATH=/home/hclimente/projects/dna2prot:${PYTHONPATH}
 
-python scripts/train.py -seed $SGE_TASK_ID -loss mse -embedding_size 16 -width 512
+python scripts/train.py -seed $SGE_TASK_ID -loss mse -weight_decay 0.01
