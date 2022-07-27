c8d8547660eb11ee22a8431ead7550efcb0f1a65
Héctor Climente-González <hclimente@gmail.com>
2022-07-27 18:38:32 +0900

    added tta results on DeepCNN

--- scripts/sge_train.sh
+++ scripts/sge_train.sh
@@ -4,9 +4,9 @@
 #$ -j y
 #$ -jc gpu-container_g8.24h
 #$ -ac d=nvcr-pytorch-2204
-#$ -t 1-5
+#$ -t 1-1
 
 . /fefs/opt/dgx/env_set/nvcr-pytorch-2204.sh
 export PYTHONPATH=/home/hclimente/projects/dna2prot:${PYTHONPATH}
 
-python scripts/train.py -seed $SGE_TASK_ID -loss mse -kernel_size 7 -weight_decay 0.01
+python scripts/train.py -seed $SGE_TASK_ID -loss mse -kernel_size 7 -weight_decay 0.01 -n_mutations 2
