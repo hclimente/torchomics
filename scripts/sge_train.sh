5dab1072b245b9f47160cfa08195361f81719127
Héctor Climente-González <hclimente@gmail.com>
2022-07-28 15:37:10 +0900

    added convnext results with tta

--- scripts/sge_train.sh
+++ scripts/sge_train.sh
@@ -4,9 +4,9 @@
 #$ -j y
 #$ -jc gpu-container_g8.24h
 #$ -ac d=nvcr-pytorch-2204
-#$ -t 1-1
+#$ -t 2-5
 
 . /fefs/opt/dgx/env_set/nvcr-pytorch-2204.sh
 export PYTHONPATH=/home/hclimente/projects/dna2prot:${PYTHONPATH}
 
-python scripts/train.py -seed $SGE_TASK_ID -loss mse -kernel_size 7 -weight_decay 0.01 -n_mutations 2
+python scripts/train.py -seed $SGE_TASK_ID -loss mse -kernel_size 15 -layers 3 9 9 3 -n_mutations 1
